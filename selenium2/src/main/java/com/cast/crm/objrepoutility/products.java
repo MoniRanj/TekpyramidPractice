e166a65240cacae833a7bb48b80edbd6880eff0b
MoniRanj <ranj.monica@gmail.com>
2025-01-31 18:04:07 +0530

    added new pom class

---
+++ selenium2/src/main/java/com/cast/crm/objrepoutility/products.java
@@ -0,0 +1,9 @@
+package com.cast.crm.objrepoutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class products {
+@FindBy(xpath="//input[@alt='Create Product...']")
+private WebElement createproductimgbtn;
+}
